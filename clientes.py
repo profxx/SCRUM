555e775006065743eb3fa9a560c0f67b033f5f52
Emili Calixto <81389639+Emilii77@users.noreply.github.com>
2021-05-31 10:43:48 -0300

    Update clientes.py

--- clientes.py
+++ clientes.py
@@ -26,34 +26,34 @@ class App(Funcs, Relatorios, Validadores):
       self.root.resizable(True, True)
       self.root.minsize (width = 700, height = 500) 
       self.root.iconbitmap("icocli.ico")
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    def frames_da_tela(self):
+      self.frame_1 = Frame(
+        self.root,
+        bd = 4, # Borda
+        bg = "#363636", # Cor de fundo (background color) highlightbackground "#836FFF",
+        highlightbackground = "#836FFF",
+        highlightthickness = 1)
+      self.frame_1.place(
+        relx = 0.01,
+        rely = 0.02,
+        relwidth = 0.98,
+        relheight = 0.46)
+      self.frame_2 Frame(
+        self.root,
+        bd = 4,
+        bg = "#363636",
+        highlightbackground = "#836FFF",
+        highlightthickness = 1)
+      self.frame_2.place(
+        relx=0.01,
+        rely=0.5,
+        relwidth=0.98,
+        relheight=0.46)
+    def widgets_framel(self):
+      # Abas
+      self.abas = ttk.Notebook(self.frame_1)
+      self.abal = Frame(self.abas)
+      self.aba2 = Frame(self.abas)
     
     
     
@@ -179,5 +179,6 @@ class App(Funcs, Relatorios, Validadores):
         filemenu2.add_command(label="Ficha do Cliente", command=self.geraRelatCliente) 
     def validaEntradas(self): 
         self.vcmd2 = (self.root.register(self.validate_entry2), "%P")
+
 # Programa Principal
 App()
